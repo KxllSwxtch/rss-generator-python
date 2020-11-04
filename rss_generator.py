09b0e8950f94a889d30b7736db7e4d75cb1c0f01
Dmitriy Shin <dimashin9@gmail.com>
2020-11-03 23:32:12 -0500

    added feedparser

--- rss_generator.py
+++ rss_generator.py
@@ -1,8 +1,17 @@
-import os
+import feedparser
+
+url = 'https://t.me/s/netflix'
 
 
 def main():
-    pass
+    try:
+        # url_to_parse = str(input('Enter the url to get the RSS: '))
+        parsed_feed = feedparser.parse(url)
+        entries = parsed_feed['entries']
+        feed = parsed_feed['feed']
+        print(feed['summary'])
+    except:
+        print("An error occured")
 
 
 if __name__ == "__main__":
